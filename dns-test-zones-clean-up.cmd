c02508fcdecd74c10636497a7c8b588e05ddb624
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-05-14 12:23:25 +0200

    Initial commit

--- dns-test-zones-clean-up.cmd
+++ dns-test-zones-clean-up.cmd
@@ -7,29 +7,27 @@ echo -----------------------------
 
 echo DNS ZONES All List
 del /q "dns-zones-all-list\.terraform.lock.hcl"
-del /q "dns-zones-all-list\terraform.tfstate"
+del /q "dns-zones-all-list\terraform.tfstate.*"
 rmdir /s /q "dns-zones-all-list\.terraform"
 
 echo DNS ZONES Lookup Create
 del /q "dns-zones-lookup-create\.terraform.lock.hcl"
-del /q "dns-zones-lookup-create\terraform.tfstate"
-del /q "dns-zones-lookup-create\terraform.tfstate.backup"
+del /q "dns-zones-lookup-create\terraform.tfstate.*"
 rmdir /s /q "dns-zones-lookup-create\.terraform"
 
 echo DNS ZONES Lookup List
 del /q "dns-zones-lookup-list\.terraform.lock.hcl"
-del /q "dns-zones-lookup-list\terraform.tfstate"
+del /q "dns-zones-lookup-list\terraform.tfstate.*"
 rmdir /s /q "dns-zones-lookup-list\.terraform"
 
 echo DNS ZONES Reverse Lookup Create
 del /q "dns-zones-reverse-lookup-create\.terraform.lock.hcl"
-del /q "dns-zones-reverse-lookup-create\terraform.tfstate"
-del /q "dns-zones-reverse-lookup-create\terraform.tfstate.backup"
+del /q "dns-zones-reverse-lookup-create\terraform.tfstate.*"
 rmdir /s /q "dns-zones-reverse-lookup-create\.terraform"
 
 echo DNS ZONES Reverse Lookup List
 del /q "dns-zones-reverse-lookup-list\.terraform.lock.hcl"
-del /q "dns-zones-reverse-lookup-list\terraform.tfstate"
+del /q "dns-zones-reverse-lookup-list\terraform.tfstate.*"
 rmdir /s /q "dns-zones-reverse-lookup-list\.terraform"
 
 echo -----------------------------
